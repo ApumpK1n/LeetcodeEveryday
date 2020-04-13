607d1898c6f1262fe67d2b95454c062b6bd30d41
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-04-13 22:33:05 +0800

    Update Twitter.cs

--- Twitter.cs
+++ Twitter.cs
@@ -1,67 +1,72 @@
-using System.Collections.Generic;
-
-public class Twitter {
+public class Twitter
+{
+    List<int> TweetList;
+    Dictionary<int, int> TweetToUser;
+    Dictionary<int, HashSet<int>> UserToFollow;
 
     /** Initialize your data structure here. */
-
-    List<Post> m_Post = new List<Post>();
-    Dictionary<int, List<int>> follow = new Dictionary<int, List<int>>();
-    Dictionary<int, List<Post>> m_UserTweet = new Dictionary<int, List<Post>>();
-    
-    public Twitter() {
-
+    public Twitter()
+    {
+        TweetList = new List<int>();
+        TweetToUser = new Dictionary<int, int>();
+        UserToFollow = new Dictionary<int, HashSet<int>>();
     }
-    
+
     /** Compose a new tweet. */
-    public void PostTweet(int userId, int tweetId) {
-        if (this.m_UserTweet.ContainsKey(userId)){
-            List<Post> post = this.m_UserTweet[userId];
-            long time = long.Parse(DateTime.ToFileTime().ToString());
-            if (post.Contains(tweetId)){
-                post[tweetId].time = time; 
+    public void PostTweet(int userId, int tweetId)
+    {
+        TweetList.Add(tweetId);
+        TweetToUser.Add(tweetId, userId);
+    }
+
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
+    public IList<int> GetNewsFeed(int userId)
+    {
+        IList<int> res = new List<int>();
+        HashSet<int> list = UserToFollow.ContainsKey(userId) ? UserToFollow[userId] : new HashSet<int>();
+        list.Add(userId);
+        for (int i = TweetList.Count - 1; i >= 0; --i)
+        {
+            if (res.Count >= 10)
+            {
+                return res;
             }
-            else{
-                Post p = new Post();
-                p.time = time;
-                p.tweetId = tweetId;
-                post.Add(p);
+            if (list.Contains(TweetToUser[TweetList[i]]))
+            {
+                res.Add(TweetList[i]);
+            }
+        }
+        return res;
+    }
+
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public void Follow(int followerId, int followeeId)
+    {
+        if (!UserToFollow.ContainsKey(followerId))
+        {
+            HashSet<int> tmp = new HashSet<int>();
+            tmp.Add(followeeId);
+            UserToFollow.Add(followerId, tmp);
+        }
+        else
+        {
+            UserToFollow[followerId].Add(followeeId);
+        }
+    }
+
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public void Unfollow(int followerId, int followeeId)
+    {
+        if (!UserToFollow.ContainsKey(followerId))
+        {
+            return;
+        }
+        else
+        {
+            if (UserToFollow[followerId].Contains(followeeId))
+            {
+                UserToFollow[followerId].Remove(followeeId);
             }
         }
     }
-    
-    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    public IList<int> GetNewsFeed(int userId) {
-
-    }
-    
-    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-    public void Follow(int followerId, int followeeId) {
-    }
-    
-    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-    public void Unfollow(int followerId, int followeeId) {
-
-    }
 }
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter obj = new Twitter();
- * obj.PostTweet(userId,tweetId);
- * IList<int> param_2 = obj.GetNewsFeed(userId);
- * obj.Follow(followerId,followeeId);
- * obj.Unfollow(followerId,followeeId);
- */
-
- public class Post{
-
-    public int tweetId{get; set;}
-
-    public long time{get; set;}
-
-    public override bool Equals(object obj){
-        if(obj == null) return false;
-        if ((int)obj != tweetId) return false;
-        return true;
-    }
- }
