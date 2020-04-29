8512197bdd269fafa3d10a1466a9309c1fe03e51
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-04-29 08:54:26 +0800

    Create FindInMountainArray.cs

---
+++ FindInMountainArray.cs
@@ -0,0 +1,17 @@
+// 1095.山脉数组中查找目标值
+// 二分查找
+
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *     public int Get(int index) {}
+ *     public int Length() {}
+ * }
+ */
+
+class Solution {
+    public int FindInMountainArray(int target, MountainArray mountainArr) {
+        
+    }
+}
