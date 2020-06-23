d4e4f39eb67503e62a61f9338f3995be5bcf5381
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-06-23 09:06:19 +0800

    Create AddBinary.cs

---
+++ June/AddBinary.cs
@@ -0,0 +1,26 @@
+// 67. 二进制求和
+
+/*
+给你两个二进制字符串，返回它们的和（用二进制表示）。
+
+输入为 非空 字符串且只包含数字 1 和 0。
+*/
+
+public class Solution {
+    public string AddBinary(string a, string b) {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        
+        string ret = "";
+        int jinwei = 0;
+        while(i >= 0 && j >= 0){
+            int num = int.Parse(a[i]) + int.Parse(b[j]) + jinwei;
+            ret += num % 2;
+            jinwei = num / 2;
+            i--;
+            j--;
+        }
+
+        
+    }
+}
