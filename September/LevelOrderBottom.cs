22f519b273279c766c5d5b76670507315f495f33
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-09-06 21:08:33 +0800

    Create LevelOrderBottom.cs

---
+++ September/LevelOrderBottom.cs
@@ -0,0 +1,50 @@
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+ //107. 二叉树的层次遍历 II
+
+ /*
+ 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ */
+using System.Collections.Generic;
+
+public class Solution {
+    public IList<IList<int>> LevelOrderBottom(TreeNode root) {
+
+        IList<IList<int>> res = new List<IList<int>>();
+        if (root == null) return res;
+        Queue<List<TreeNode>> queue = new Queue<List<TreeNode>>();
+        List<TreeNode> start = new List<TreeNode>();
+        start.Add(root);
+        queue.Enqueue(start);
+        while(queue.Count > 0)
+        {
+            List<TreeNode> lstTreeNode = queue.Dequeue();
+            List<int> num = new List<int>();
+            List<TreeNode> next = new List<TreeNode>();
+            foreach(TreeNode node in lstTreeNode){
+                num.Add(node.val);
+                if (node.left != null){
+                    next.Add(node.left);
+                }
+                if (node.right != null){
+                    next.Add(node.right);
+                }
+            }
+            res.Add(num);
+            if (next.Count > 0){
+                queue.Enqueue(next);
+            }
+        }
+        return res.Reverse().ToList();
+    }
+}
