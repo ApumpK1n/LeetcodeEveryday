632ad4b4f35152e0b5f4b3dcc72948b4175fd9ac
ApumpK1n <30541379+ApumpK1n@users.noreply.github.com>
2020-09-16 09:03:56 +0800

    update

---
+++ September/InvertTree.cs
@@ -0,0 +1,23 @@
+
+
+//226. 翻转二叉树
+// 翻转一棵二叉树。
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode node = root.left;
+        root.left = InvertTree(root.right);
+        root.right = InvertTree(node);
+        return root;
+    }
+}
