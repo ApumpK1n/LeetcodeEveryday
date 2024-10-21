f5160e1809a28bc7df58082662ed3d7e57fe7d96
lis <30541379+ApumpK1n@users.noreply.github.com>
2024-10-21 19:36:23 +0800

    Update 910.SmallestRangeII.cs

--- 910.SmallestRangeII.cs
+++ 910.SmallestRangeII.cs
@@ -1,3 +1,14 @@
+/*
+910. 最小差值 II
+给你一个整数数组 nums，和一个整数 k 。
+
+对于每个下标 i（0 <= i < nums.length），将 nums[i] 变成 nums[i] + k 或 nums[i] - k 。
+
+nums 的 分数 是 nums 中最大元素和最小元素的差值。
+
+在更改每个下标对应的值之后，返回 nums 的最小 分数 。
+*/
+
 public class Solution
 {
     public int SmallestRangeII(int[] nums, int k)
